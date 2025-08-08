cfa35964cf12a0bcb93668dabf6dcfac767831b4
andreluiisdantas <andreluissousaa10@gmail.com>
2025-08-08 16:29:41 -0300

    CRUD

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path("Autores", AutoresView.as_view()),
+    path("Autores/<int:pk>", AutoresRetrieveUpdateDestroyAPIView.as_view()),
 ]
 
--- api/views.py
+++ api/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Autor
 from .serializers import AutorSerializers
 
 class AutoresView(ListCreateAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializers
+
+class AutoresRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializers
     
